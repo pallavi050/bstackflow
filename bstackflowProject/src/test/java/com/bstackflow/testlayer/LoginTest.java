c39d74641edee0c5cbb1a9fb420fc680ca9f8f2d
pallavi050 <pallavi.godse1992@gmail.com>
2024-09-11 15:32:29 +0530

    please check login details

---
+++ bstackflowProject/src/test/java/com/bstackflow/testlayer/LoginTest.java
@@ -0,0 +1,24 @@
+package com.bstackflow.testlayer;
+
+import org.testng.annotations.Test;
+
+import com.bstackflow.pagelayer.Homepage;
+import com.bstackflow.pagelayer.LoginPage;
+import com.bstackflow.testbase.Testbase;
+
+public class LoginTest extends Testbase{
+
+	@Test
+	public void VerifyRegisterWithValidDetails() throws InterruptedException
+	{
+		Homepage homepage_obj=new Homepage(driver);
+		
+		homepage_obj.SignUp();
+		Thread.sleep(2000);
+		LoginPage loginpage_obj=new LoginPage(driver);
+		Thread.sleep(2000);
+		loginpage_obj.enterUsername("demouser");
+		loginpage_obj.enterPass("testingisfun99");
+		
+	}
+}
