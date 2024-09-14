8d14dced8af82c9356a1ed1cab258450fa4ae858
pallavi050 <pallavi.godse1992@gmail.com>
2024-09-14 12:45:51 +0530

    login completed

--- bstackflowProject/src/main/java/com/bstackflow/utility/UtilClass.java
+++ bstackflowProject/src/main/java/com/bstackflow/utility/UtilClass.java
@@ -1,6 +1,7 @@
 package com.bstackflow.utility;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import com.bstackflow.testbase.Testbase;
@@ -14,5 +15,12 @@ public class UtilClass extends Testbase{
 	{
 		Select s=new Select(element);
 	  s.selectByVisibleText(text);
+	}
+	
+	public static void moveToElement(WebElement element)
+	{
+		Actions act=new Actions(driver);
+		act.moveToElement(element).build().perform();
+		element.click();
 	}
 }
